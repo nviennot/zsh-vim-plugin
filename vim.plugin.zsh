53a524e8042cbe957d7928eb29c3bd43a84bb40d
Nicolas Viennot <nicolas@viennot.biz>
2012-05-14 18:45:14 -0400

    Initial commit

---
+++ vim.plugin.zsh
@@ -0,0 +1,14 @@
+vim_commands=(vi vim gvim mvim)
+
+_run_vim_wrapper() {
+	local cmd
+	for arg in $@; do
+		cmd="$cmd \"${arg/:/\" \"+:}\""
+	done
+	eval $cmd
+}
+
+for cmd in $vim_commands; do
+	eval "function ${cmd}_wrapper () { _run_vim_wrapper $cmd \$@}"
+	alias $cmd=${cmd}_wrapper
+done
