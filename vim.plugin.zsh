1deaa116122bbdd42559fff8f0a48a540806c525
Jean-Sébastien Ney <jeansebastien.ney@gmail.com>
2013-01-28 20:57:47 +0100

    do not alias command, if it is already one

--- vim.plugin.zsh
+++ vim.plugin.zsh
@@ -10,5 +10,5 @@ _run_vim_wrapper() {
 
 for cmd in $vim_commands; do
 	eval "function ${cmd}_wrapper () { _run_vim_wrapper $cmd \$@}"
-	alias $cmd=${cmd}_wrapper
+	alias $cmd 2>&1 >/dev/null || alias $cmd=${cmd}_wrapper
 done
